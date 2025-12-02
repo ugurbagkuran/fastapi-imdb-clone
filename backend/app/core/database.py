8daf5ed539e28b5c633f2c94cdb6f4864233348f
ugurbagkuran <ugurbagkuranu@gmail.com>
2025-12-03 00:53:42 +0300

    bazı hatalar giderildi

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,24 +1,25 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from app.core.config import settings
+from typing import Optional
 
 class Database:
-    client: AsyncIOMotorClient = None
+    client: Optional[AsyncIOMotorClient] = None
 
 db = Database()
 
-async def get_database():
+async def get_database() -> AsyncIOMotorDatabase:
     """
     Diğer dosyalardan (Auth, Movies) veritabanına erişmek istediğimizde
     bu fonksiyonu çağıracağız.
     """
     return db.client[settings.DB_NAME]
 
-async def connect_to_mongo():
+async def connect_to_mongo() -> None:
     """Uygulama başlarken çalışacak"""
     db.client = AsyncIOMotorClient(settings.MONGO_URL)
     print("MongoDB Bağlantısı Başarılı!")
 
-async def close_mongo_connection():
+async def close_mongo_connection() -> None:
     """Uygulama kapanırken çalışacak"""
     db.client.close()
     print("MongoDB Bağlantısı Kapatıldı.")
